b69f4737a9b4aeb9820e043ba59eee3a370d5d55
Diz <yurasovm@gmail.com>
2024-06-27 20:22:19 +0500

    fix

--- src/components/vanilla/button/Button.ts
+++ src/components/vanilla/button/Button.ts
@@ -1,4 +1,59 @@
-export function makeButton(config): string {
-	// реализация функции
-	return `<button>button</button>`;
+import {tv} from 'tailwind-variants';
+import tvConfig from '../../../tv-configs/button.json';
+import { cn } from '../../../helpers/tailwind';
+
+const tvButton = tv(tvConfig);
+
+const {
+	base : slotBase ,
+	label : slotLabel,
+	iconWrapper : slotIconWrapper,
+	iconWrapperStart : slotIconWrapperStart,
+	iconWrapperEnd : slotIconWrapperEnd,
+	icon : slotIcon,
+} = tvButton();
+
+const defProps = {
+	label : '',
+	variant : 'primary',
+	size : '500',
+	iconStart : false,
+	iconEnd : false,
+	class : '',
+}
+
+export function makeButton( props ): string {
+	props = {...defProps, ...props};
+
+	const slotsProps = {
+		variant : props.variant,
+		size : props.size,
+		onlyIcon : !props.label || props.label == ''
+	}
+
+	return `
+		<button
+			class="${ cn( slotBase( slotsProps ), props.class ) }"
+		>
+			${
+				props.iconStart &&
+				`
+					<span class="${ cn( slotIconWrapper( slotsProps ), slotIconWrapperStart( slotsProps ) ) }">
+						<Icon className={ cn( slotIcon( slotsProps ) ) } name={iconStart} size={ 24 } />
+					</span>
+				`
+			}
+
+			${
+				props.label && `<span className=${ cn( slotLabel( slotsProps ) ) }>${ props.label }</span>`
+			}
+
+			${
+				props.iconEnd &&
+				`<span className=${ cn( slotIconWrapper( slotsProps ), slotIconWrapperEnd( slotsProps )  ) }>
+					<Icon className=${ cn( slotIcon( slotsProps ) ) } name="${ props.iconEnd }" size="24" />
+				</span>`
+			}
+		</button>
+	`;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,41 +5,38 @@ import { resolve } from 'path';
 import { copyFileSync, mkdirSync, readdirSync } from 'fs';
 
 export default defineConfig({
-  plugins: [
-	react(),
-	dts(),
-	{
-		name: 'copy-tv-configs',
-		generateBundle() {
-		  const srcDir = resolve(__dirname, 'src/tv-configs');
-		  const outDir = resolve(__dirname, 'dist/tv-configs');
-		  
-		  // Создаем директорию, если она не существует
-		  mkdirSync(outDir, { recursive: true });
-		  
-		  // Копируем все JSON файлы
-		  readdirSync(srcDir).forEach(file => {
-			if (file.endsWith('.json')) {
-			  copyFileSync(resolve(srcDir, file), resolve(outDir, file));
+	plugins: [
+		react(),
+		dts(),
+		{
+			name: 'copy-tv-configs',
+			generateBundle() {
+				const srcDir = resolve(__dirname, 'src/tv-configs');
+				const outDir = resolve(__dirname, 'dist/tv-configs');
+				mkdirSync(outDir, { recursive: true });
+
+				readdirSync(srcDir).forEach( file => {
+					if ( file.endsWith('.json') ) {
+						copyFileSync( resolve( srcDir, file ), resolve( outDir, file ) );
+					}
+				});
 			}
-		  });
 		}
-	  }
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: '4PartnersKit',
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
+	],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/index.ts'),
+			name: '4PartnersKit',
+			fileName: (format) => `index.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+	},
 });
