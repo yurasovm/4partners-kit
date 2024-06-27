b1fbb22f8f5f6e5184544bbf506a54a06ff0e7bc
Diz <yurasovm@gmail.com>
2024-06-27 16:31:18 +0500

    fix

--- src/tailwind/index.ts
+++ src/tailwind/index.ts
@@ -6,8 +6,8 @@ import typographyUtilitiesPlugin from './plugins/typography.ts';
 import roundedUtilitiesPlugin from './plugins/rounded.ts';
 
 // Configs
-import borderRadiusConfig from './plugins/configs/border-radius.json' assert { type: 'json' };
-import colorsConfig from './plugins/configs/colors.json';
+import borderRadiusConfig from './configs/border-radius.json' assert { type: 'json' };
+import colorsConfig from './configs/colors.json';
 
 export default ( customizer ) => {
 	const builder = new TailwindConfigBuilder();
