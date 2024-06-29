279a5478bcded79e56b533350b0dd9d55c7b23d6
Diz <yurasovm@gmail.com>
2024-06-29 16:23:22 +0500

    fix

--- src/helpers/tailwind.ts
+++ src/helpers/tailwind.ts
@@ -1,6 +1,6 @@
 import {twMerge} from 'tailwind-merge';
 import clsx from 'clsx';
 
-export function cn(...inputs) {
+export function cn(...inputs: (string | undefined)[]) {
 	return twMerge(clsx(inputs));
 }
